30aa127b5655b6068d7c53014020a1bd71ea2e8d
Martin Hernandez <7607920511m.h@gmail.com>
2016-04-19 22:18:10 -0700

    Missing 202

---
+++ Project/template.cpp
@@ -0,0 +1,25 @@
+/**
+File: sine.cpp
+Created: 10/07/15
+Author: Martin Hernandez
+email: 7607920511m.h@gmail.com
+Description: 
+*/
+
+//Add libraries here
+#include<iostream>
+#include<cmath>
+#include<fstream>
+
+using namespace std;
+
+//Declare functions here
+
+//Declare global variables here
+int main() 
+{        
+
+    return 0;
+} 
+
+//Insert user defined funtions here
