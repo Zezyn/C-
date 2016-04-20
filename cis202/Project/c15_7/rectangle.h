abcfaeedcc1b41a1d20b38e97ac026e66b8d5159
Martin Hernandez <7607920511m.h@gmail.com>
2016-04-19 22:40:03 -0700

    Changes

---
+++ cis202/Project/c15_7/rectangle.h
@@ -0,0 +1,18 @@
+/*
+    Filename: rectangle.h
+    Problem:
+    Author: Martin Hernandez
+    Email: 7607920511m.h@gmail.com
+    Date: 03/08/2016
+    Description: Rectangle header
+ */
+
+#ifndef rectangle
+#define rectangle
+
+#include "figure.h"
+
+class Rectangle : public Figure {
+};
+
+#endif
