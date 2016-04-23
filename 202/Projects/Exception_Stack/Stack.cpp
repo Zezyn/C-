17b9505f8ce0c06b30d8eaf32e9a9f305dade7cf
Martin Hernandez <7607920511m.h@gmail.com>
2016-04-23 16:25:13 -0700

    Organizing and working on Exception Stack

---
+++ 202/Projects/Exception_Stack/Stack.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstddef>
+#include "stack.h"
+
+using namespace std;
+
+Stack::Stack() : top(NULL){}
+
+Stack::Stack(const Stack& a_stack) {
+    if(a_stack.top == NULL) { top = NULL; }
+    else {
+        StackFramePtr temp = a_stack.top; //Temp moves through nodes
+        StackFramePtr end; //End of stack
+
+        //First node filled with data, new nodes after first
+        end = new StackFrame;
+        end->date = temp->data;
+        top = end;
+
+        temp - temp->link;
+        while(temp != NULL) {
+            end->link - new StackFrame;
+            end = end->link;
+            end->data = temp->data;
+            temp = temp->link;
+        }
+        end->link = NULL;
+    }
+}
+
+Stack::~Stack() {
+    char next;
+    while(! empty()) { next = pop(); } //Pop calls delete
+}
+
+bool Stack::empty() const { return (top == NULL); }
+
+void Stack::push(char symbol) {
+    StackFramePtr temp = new StackFrame;
+    temp->data = symbol;
+    temp->link = top;
+    top = temp;
+}
+
+char Stack::pop() {
+
+    if(empty()) { throw Error(); exit(1); } //THROWING EXCEPTION HERE 
+                        
+    char result = top->data;
+    StackFramePtr tempPtr;
+    tempPtr = top;
+    top = top->link;
+
+    delete tempPtr;
+
+    return result;
+}
+
+
---
+++ 202/Projects/Exception_Stack/Stack.h
@@ -0,0 +1,22 @@
+#ifndef STACK_H
+#define STACK_H
+
+struct StackFrame {
+    char data;
+    StackFrame *link;
+};
+
+typedef StackFrame *StackframePtr;
+
+class Stack {
+    public:
+        Stack(); //Initializes empty stack
+        Stack(const Stack* a_stack); //Copy Constructor
+        ~Stack(); //Destroy stack and return memory
+        void push(char the_symbol); //Add to stack
+        char pop(); //Pop off stack
+        bool empty() const; //True is stack is empty
+    private:
+        StackFramePtr top;
+};
+#endif
