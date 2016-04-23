690eac6f9c873fb3f8426f716f0d53e8bfaad1f5
Martin Hernandez <7607920511m.h@gmail.com>
2016-04-23 13:45:09 -0700

    Array not giving Value

--- 202/Projects/c16_4/main.cpp
+++ 202/Projects/c16_4/main.cpp
@@ -1,10 +1,10 @@
 /*
     Filename: main.cpp
-    Problem:
+    Problem:The Array gives me the address instead of the value
     Author: Martin Hernandez 
     Email: 7607920511m.h@gmail.com
     Date: 04/21/2016
-    Description: 
+    Description: Throw an exception if out of range
 */
 
 #include <iostream>
@@ -15,16 +15,17 @@ using namespace std;
 
 void use_array() throw(ArrayOutOfRangeError) {
     CheckedArray A;//<-pass it the size
-    //A[100] = 4; //this should throw an exception in your code after you comment it out.
-    std::cout << A[1] << std::endl; 
+    int i = A.get_index(); //Get index from user
+    //A[100] = 4; //Should throw exception TEST LINE
+    std::cout << "Value of index: " << i << " is " 
+    << A[i] << std::endl; //Output index from user 
 }
 
 int main() {
+
     try { use_array(); }
     catch(const ArrayOutOfRangeError e) 
-        { std::cout << "Error in program, check the log file" ;
-            //<< std::endl; log << e; }
-        }
+        { std::cout << "Error in program, check the log file" ; }
+        
     return 0;
 }
-
