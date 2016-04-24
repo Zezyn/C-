d36ba7a9e89645385488e2bced19c9501ce2f47a
Martin Hernandez <7607920511m.h@gmail.com>
2016-04-24 15:29:58 -0700

    Stack Works

--- 202/Projects/Exception_Stack/main.cpp
+++ 202/Projects/Exception_Stack/main.cpp
@@ -12,8 +12,24 @@
 using namespace std;
 
 int main() {
+    char ans;
+    char next;
 
     Stack s;
-    
+     
+    do{
+        std::cout << "Enter a name -> ";
+        cin.get(next);
+        while(next != '\n') {
+            s.push(next);
+            cin.get(next);
+        }
+        while(! s.empty()) { std::cout << s.pop(); }
+
+        std::cout << "\n\nAnother?(Y/n): ";
+        cin.get(ans);
+        cin.ignore(10000, '\n');
+    } while (ans != 'n' && ans != 'N');
+
     return 0;
 }
