bf3a1ad5febc13753ac848d471200ce500875c4f
Martin Hernandez <7607920511m.h@gmail.com>
2016-05-05 14:41:06 -0700

    Book Database

---
+++ 202/Projects/Book_Database/database.h
@@ -0,0 +1,14 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+class database {
+    public:
+        void get_data();
+    private:
+        string author;
+        string title;
+        string date;
+};
+
+#endif
+
---
+++ 202/Projects/Book_Database/library.h
@@ -0,0 +1,10 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+//Create a vector for books
+
+class books {
+    public:
+        void store_books();
+    private:
+        vector<int> books;
+#endif
