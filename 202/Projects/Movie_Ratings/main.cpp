c9f37cb9599f3e3ed1dcc20323a14bc30469600d
Martin Hernandez <7607920511m.h@gmail.com>
2016-05-08 21:07:36 -0700

    Unable to create maps

--- 202/Projects/Movie_Ratings/main.cpp
+++ 202/Projects/Movie_Ratings/main.cpp
@@ -1,6 +1,6 @@
 /*
 Filename: main.cpp
-Problem:
+Problem: Unable to create maps from vector or variables only manually enter keys which isn't efficient
 Author: Martin Hernandez
 Email: 7607920511m.h@gmail.com
 Date: 05/07/2016
@@ -46,12 +46,18 @@ int main() {
     }
     
     //Input into map
-    string temp_check;
+    string temp_check; //Used as middle man between array and map
     temp_check = temp[1];    
-    
+
+    //Insert vector into map
+    //Only seems to work by manually entering keys won't accept variables
+
+    //list.insert(pair<string, vector<string>>(temp[1],temp[2]));
+    list[temp[1], temp[2]];
     //ratings.insert(temp[0]);                                      //DW
     //ratings["Movie 1 Rating"] = temp[0];
-    //ratings[temp[1]] = temp[0];                                   //Doesn't work***
+//    ratings[temp[1]] = temp[0];                                   //Doesn't work***
+//    ratings.insert(pair<string, string>(temp[1], temp[0]));         //STILL GIVES BAD OUTPUT
     //movies["Movie 1 Title"] = temp[1]; //ONLY LINE THAT WORKS
     //movies[temp[1]] = temp[1];
     //avg[temp[1]] = temp[2];
@@ -66,19 +72,22 @@ int main() {
     */
 
     //Print map
-    map<string, string>::const_iterator iter;                       //Creates Iterator
     
-    for(iter = ratings.begin(); iter != ratings.end(); iter++) {    //Iterator for ratings
-        cout << iter->first << " - " << iter->second << endl;
-    }
+    map<string, string>::const_iterator iter_l;                       //Creates Iterator
+    for(iter_l = list.begin(); iter_l != list.end(); iter_l++)      //Iterator for list
+       { cout << iter_l->first << " - " << iter_l->second << endl; }
     
-    for(iter = movies.begin(); iter != movies.end(); iter++) {      //Iterator for movies
-        cout << iter->first << " - " << iter->second << endl; 
-    }
-    for(iter = avg.begin(); iter != avg.end(); iter++) {            //Iterator for avg
-        cout << iter->first << " - " << iter->second << endl;
-    }
+    map<string, string>::const_iterator iter_r;                       //Creates Iterator
+    for(iter_r = ratings.begin(); iter_r != ratings.end(); iter_r++)      //Iterator for ratings
+       { cout << iter_r->first << " - " << iter_r->second << endl; }
+    
+    map<string, string>::const_iterator iter_m;                       //Creates Iterator
+    for(iter_m = movies.begin(); iter_m != movies.end(); iter_m++)        //Iterator for movies
+       { cout << iter_m->first << " - " << iter_m->second << endl; }
 
+    map<string, string>::const_iterator iter_a;                       //Creates Iterator
+    for(iter_a = avg.begin(); iter_a != avg.end(); iter_a++)              //Iterator for avg
+       { cout << iter_a->first << " - " << iter_a->second << endl; }
 
     return 0;
 }
