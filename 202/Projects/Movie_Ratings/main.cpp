46e3976e77a1863e1a257c7b5d26cc6a7aa7f148
Martin Hernandez <7607920511m.h@gmail.com>
2016-05-12 16:36:52 -0700

    It WOrks

--- 202/Projects/Movie_Ratings/main.cpp
+++ 202/Projects/Movie_Ratings/main.cpp
@@ -19,75 +19,46 @@ using namespace std;
 int main() {
     
     //Create maps
-    map<string, string> list;
-    map<string, string> ratings;
-    map<string, string> movies;
-    map<string, string> avg;
+    map<string, vector<double> > database; 
     
     //Input from file
-    ifstream ifs("movies.txt");
-    string buffer;
-    vector<std::string> temp;
+    ifstream ifs;
+    ifs.open("movies.txt");
+    string buffer, atemp1, atemp2;
 
     if(ifs.fail()) {
         cout << "Problem reading file" << endl;
         exit(1);
     }
 
-
-    while(!ifs.eof()) {
-
-        getline(ifs,buffer);                                        //Gets whole line
-        temp.push_back(buffer);                                     //Control the flow of information
-
-        //ifs >> buffer;                                            //Gets word by word
-        //cout << "Buffer: " << endl << buffer << endl;             //Prints buffer
-
+    getline(ifs, buffer);
+    while(!ifs.eof())
+    {
+        getline(ifs, atemp1);
+        getline(ifs, atemp2);
+        if(! atemp1.empty() && ! atemp2.empty())
+        { 
+           // cout << atemp1 << endl;
+           // cout << atemp1 << " : " << atemp1.c_str() << endl;
+          database[string(atemp1.c_str())].push_back(static_cast<double>(stoi(atemp2)));
+        }
     }
-    
-    //Input into map
-    string temp_check; //Used as middle man between array and map
-    temp_check = temp[1];    
-
-    //Insert vector into map
-    //Only seems to work by manually entering keys won't accept variables
-
-    //list.insert(pair<string, vector<string>>(temp[1],temp[2]));
-    list[temp[1], temp[2]];
-    //ratings.insert(temp[0]);                                      //DW
-    //ratings["Movie 1 Rating"] = temp[0];
-//    ratings[temp[1]] = temp[0];                                   //Doesn't work***
-//    ratings.insert(pair<string, string>(temp[1], temp[0]));         //STILL GIVES BAD OUTPUT
-    //movies["Movie 1 Title"] = temp[1]; //ONLY LINE THAT WORKS
-    //movies[temp[1]] = temp[1];
-    //avg[temp[1]] = temp[2];
-    //avg["Movie 1 Average"] = temp[2];  //ONLY LINE THAT WORKS
-
-//    cout << endl << "Vector: " << temp[1];                        //Test line for vector
-
-    /*
-    for(int z=0; z<temp.size(); z++) {                              //Prints vector
-        cout << temp[z] << endl;
-    }   
-    */
-
     //Print map
-    
-    map<string, string>::const_iterator iter_l;                       //Creates Iterator
-    for(iter_l = list.begin(); iter_l != list.end(); iter_l++)      //Iterator for list
-       { cout << iter_l->first << " - " << iter_l->second << endl; }
-    
-    map<string, string>::const_iterator iter_r;                       //Creates Iterator
-    for(iter_r = ratings.begin(); iter_r != ratings.end(); iter_r++)      //Iterator for ratings
-       { cout << iter_r->first << " - " << iter_r->second << endl; }
-    
-    map<string, string>::const_iterator iter_m;                       //Creates Iterator
-    for(iter_m = movies.begin(); iter_m != movies.end(); iter_m++)        //Iterator for movies
-       { cout << iter_m->first << " - " << iter_m->second << endl; }
+    double num;
+    map<string, vector<double> >::const_iterator iter_a;                       //Creates Iterator
+    vector<double>::const_iterator iter_b;                                     //Creates Iterator
+    for(iter_a = database.begin(); iter_a != database.end(); iter_a++) {        //Iterator for avg 
+        num = 0.0;
+        for(iter_b = iter_a->second.begin(); iter_b != iter_a->second.end(); iter_b++)
+        { 
+            num=num+(*iter_b); 
+        }
 
-    map<string, string>::const_iterator iter_a;                       //Creates Iterator
-    for(iter_a = avg.begin(); iter_a != avg.end(); iter_a++)              //Iterator for avg
-       { cout << iter_a->first << " - " << iter_a->second << endl; }
+        cout << iter_a->first.c_str();
+        cout << iter_a->first << ": " << "Number of reviews: " 
+        << iter_a->second.size() << ", " << "Average rating: " << (num/iter_a->second.size()) << endl;
+     }
+    
 
     return 0;
 }
