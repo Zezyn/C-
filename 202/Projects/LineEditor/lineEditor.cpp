95f65244b9c01fe7b3c7fd5730cfbc1788290f79
Zezyn <7607920511m.h@gmail.com>
2016-06-07 12:52:21 -0700

    Working on lineEditor

--- 202/Projects/LineEditor/lineEditor.cpp
+++ 202/Projects/LineEditor/lineEditor.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
+
 using namespace std;
 
 void lineEditor::newText()
@@ -16,12 +17,12 @@ void lineEditor::newText()
 void lineEditor::openText()
 {
     cout << "Enter filename: ";
-    std::string filename;
-    cin >> filename;
+    std::string filen;
+    cin >> filen;
     cout << endl;
 
-    char filename{256};
-    cin.getline(filename, 256);  // <- to allow whitespaces
+    std::string filename;
+    getline(cin, filename);  // <- to allow whitespaces
     std::fstream f(filename, ios::in);
 // do something with the file
     f.close();
@@ -33,12 +34,48 @@ void lineEditor::openText()
 
 void lineEditor::saveText()
 {
+    std::string filename;
+    std::cout << "Please enter a file name (ex:filename.txt): ";
+    std::cin >> filename;
 
+    //for(std::vector<int>::iterator it = text.begin(); it != text.end();it++)
+      //  cout <<*it<<std::endl;
+
+    for(int i=0;i<5;i++)
+        { cout << text[i] << endl; }
+    Text();
 }
 
 void lineEditor::Text()
 {
     std::string line;
-    getline(cin, line);
-    text.push_back(line);
+    int linenum = 2;
+
+    std::cout << endl << "*****************************************************" << endl;
+    std::cout << "              Line        Editor                     " << endl;
+    std::cout << "*****************************************************" << endl;
+    std::cout << "!N = Blank Page | !O = Open | !S = Save | !Q to quit" << endl;
+    std::cout << "*****************************************************" << endl;
+    std::cout << "1> ";
+
+    while(getline(cin, line))
+    {
+        if (line == "!Q") {
+            exit(1);
+        }
+        else if (line == "!N") {
+            newText();
+        }
+        else if (line == "!O") {
+            openText();
+        }
+        else if (line == "!S") {
+            saveText();
+        }
+        else {
+            cout << linenum << "> ";
+            text.push_back(line);
+            linenum += 1;
+        }
+    }
 }
--- 202/Projects/LineEditor/main.cpp
+++ 202/Projects/LineEditor/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include "lineEditor.h"
 
 using namespace std;
 
 int main() {
 
+    lineEditor l;
+    l.Text();
     return 0;
 }
